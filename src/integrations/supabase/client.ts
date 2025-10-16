9a038e2216e2ce1dce65bd34eefef73b0c9f6840
verticalsystems11 <admin@growthcreators.ai>
2025-10-16 21:19:14 +0530

    bug fix

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://scwcuibrijawazbpglwz.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjd2N1aWJyaWphd2F6YnBnbHd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMjY5MDgsImV4cCI6MjA2MzYwMjkwOH0.KSVbzMGv2dNODuQNmU_6m02kHP-YOxUxlGgRQGCJiPQ";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
